31e9848e6a7f0c0c6966bfa4bc15b6c99001a83c
Lurius-tharn <sajous.davenese@gmail.com>
2022-06-05 18:58:27 +0200

    test retiré

--- Assets/Tests/GameStartTest.cs
+++ Assets/Tests/GameStartTest.cs
@@ -1,39 +1,19 @@
 using System.Collections;
-using NUnit.Framework;
-using TMPro;
-using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
 
 public class Please : InputTestFixture
 {
-    private TextMeshProUGUI _textMeshProUGUI;
-    private Keyboard keyboard;
-
     public override void Setup()
     {
         base.Setup();
         SceneManager.LoadScene("Scenes/MainScreen");
-        
-
-        keyboard = InputSystem.AddDevice<Keyboard>();
-        Debug.Log(keyboard);
     }
 
     [UnityTest]
     public IEnumerator TestGameStart()
     {
-       
-        yield return new WaitForSeconds(2f);
-
-        
-    }
-
-    [UnityTest]
-    public IEnumerator TestGameStart_texte_devrait_clignoter()
-    {
-      
-        yield return new WaitForSeconds(2f);
+        yield return null;
     }
 }
