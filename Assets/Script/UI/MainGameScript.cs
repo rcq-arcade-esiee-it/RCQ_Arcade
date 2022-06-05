e555363bbf4779bfa6d2c51c37af47fbe73595fa
Lurius-tharn <69853511+Lurius-tharn@users.noreply.github.com>
2022-06-06 00:40:25 +0200

    Ajout du premier screen, ajout de TextMeshPro (#2)
    * Ajout du premier screen, ajout de TextMeshPro, InputSystem
    
    * test du coverage: supression des tests
    
    * test retiré
    
    * Update README.md
    
    * ajout test

---
+++ Assets/Script/UI/MainGameScript.cs
@@ -0,0 +1,48 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class MainGameScript : MonoBehaviour
+{
+    private AudioSource audioWhistle;
+
+    // Start is called before the first frame update
+    private double timer;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        audioWhistle = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        blinkText();
+
+        if (Keyboard.current.anyKey.wasPressedThisFrame
+           )
+        {
+            if (!audioWhistle.isPlaying) audioWhistle.Play();
+            Invoke("ChangeMenu", 1);
+        }
+    }
+
+    private void blinkText()
+    {
+        timer = timer + Time.deltaTime * 0.75;
+        // Clignotement text ( a améliorer)
+        if (timer >= 0.5) GetComponent<TextMeshProUGUI>().color = new Color32(46, 124, 51, 255);
+        if (timer >= 1)
+        {
+            GetComponent<TextMeshProUGUI>().color = new Color32(255, 255, 255, 0);
+            timer = 0;
+        }
+    }
+
+    private void ChangeMenu()
+    {
+        SceneManager.LoadScene("Scenes/GameMenu", LoadSceneMode.Single);
+    }
+}
---
+++ Assets/Tests/GameStartTest.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using NUnit.Framework;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+public class Please : InputTestFixture
+{
+    private TextMeshProUGUI _textMeshProUGUI;
+    private Keyboard keyboard;
+
+    public override void Setup()
+    {
+        base.Setup();
+        SceneManager.LoadScene("Scenes/MainScreen");
+
+
+        keyboard = InputSystem.AddDevice<Keyboard>();
+        Debug.Log(keyboard);
+    }
+
+    [UnityTest]
+    public IEnumerator TestGameStart()
+    {
+        var sceneName = SceneManager.GetActiveScene().name;
+        Debug.Log(SceneManager.GetActiveScene().path);
+
+        Assert.That(sceneName, Is.EqualTo("MainScreen"));
+        // Cette fonction simule un joueur appuyant sur la touche a de son clavier
+        Press(keyboard.aKey);
+        yield return new WaitForSeconds(2f);
+
+        sceneName = SceneManager.GetActiveScene().name;
+        Assert.That(sceneName, Is.EqualTo("GameMenu"));
+    }
+
+    [UnityTest]
+    public IEnumerator TestGameStart_texte_devrait_clignoter()
+    {
+        _textMeshProUGUI = GameObject.Find("Canvas/TextTypeStart").GetComponent<TextMeshProUGUI>();
+        var firstColor = _textMeshProUGUI.color;
+        yield return new WaitForSeconds(2f);
+        Assert.That(firstColor, Is.Not.EqualTo(_textMeshProUGUI.color));
+    }
+}
