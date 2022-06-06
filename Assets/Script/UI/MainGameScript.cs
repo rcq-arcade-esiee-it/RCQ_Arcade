1a549abd0b9ce57ac7689309c6e168398b879254
Lurius-tharn <69853511+Lurius-tharn@users.noreply.github.com>
2022-06-06 21:56:29 +0200

    modifs input system

--- Assets/Script/UI/MainGameScript.cs
+++ Assets/Script/UI/MainGameScript.cs
@@ -1,6 +1,7 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
 using UnityEngine.SceneManagement;
 
 public class MainGameScript : MonoBehaviour
