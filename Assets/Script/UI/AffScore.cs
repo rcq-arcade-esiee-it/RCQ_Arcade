2289dc1e8587cf8ad53996b0f3dbe374bf325509
socrux <socruxx@gmail.com>
2022-06-28 02:36:24 +0200

    Commencement

---
+++ Assets/Script/UI/AffScore.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO; 
+using System.Linq;
+using UnityEngine.UI;
+public class AffScore : MonoBehaviour
+{
+    void Start()
+    {
+	    string readFromFilePath = Application.dataPath + "/Script/UI/" + "score_jeu_un" + ".txt";
+    }
+    void Update()
+    {
+        
+    }
+    
+}
